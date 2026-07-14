ccc896485c5189d979943a9b9e8a3b316413effb
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-15 00:01:47 +0530

    feat: replace cube mesh with robot GLTF model and add ambient lighting

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { useTexture } from '@react-three/drei';
+import { useGLTF, useTexture } from '@react-three/drei';
 import { useFrame, useLoader } from '@react-three/fiber';
 import React, { useRef } from 'react'
 import * as THREE from "three"
@@ -8,7 +8,7 @@ const Experience = () => {
     const cubeRef = useRef(null)
 
     useFrame((state, delta) => {
-        cubeRef.current.rotation.y += delta
+        // cubeRef.current.rotation.y += delta
 
     })
 
@@ -19,12 +19,18 @@ const Experience = () => {
       "https://images.unsplash.com/photo-1496433998859-da21e208bd42?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     )
 
+    const model = useGLTF("./robot.glb")
+
   return (
     <>
-      <mesh ref={cubeRef}>
+      {/* <mesh ref={cubeRef}>
         <boxGeometry />
         <meshBasicMaterial map={dreiTexture} />
-      </mesh>
+      </mesh> */}
+
+      <ambientLight intensity={3} color={"#fffff"} />
+
+      <primitive object={model.scene} position={[0, -1, -4]} />
     </>
   );
 };

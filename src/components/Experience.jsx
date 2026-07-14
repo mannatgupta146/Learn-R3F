e52a8d2913363268776232bfbc6fc7209cf04f83
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-15 00:56:33 +0530

    feat: implement randomized box instances using matcap material and texture asset

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, useGLTF, useTexture } from '@react-three/drei';
+import { Environment, Instance, Instances, useGLTF, useTexture } from '@react-three/drei';
 import { useFrame, useLoader } from '@react-three/fiber';
 import React, { useRef } from 'react'
 import * as THREE from "three"
@@ -19,20 +19,44 @@ const Experience = () => {
       "https://images.unsplash.com/photo-1496433998859-da21e208bd42?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     )
 
+    const { matcap } = useTexture({
+      matcap: "./matcap.png"
+    })
+1
     const model = useGLTF("./robot.glb")
+
+    const handleClick = () => {
+      cubeRef.current.material.color.set("green")
+    }
 
   return (
     <>
-      <mesh ref={cubeRef}>
+      {/* <mesh onClick={handleClick} ref={cubeRef}>
         <boxGeometry />
         <meshStandardMaterial roughness={0.01} metalness={0.9} color={"red"} />
-      </mesh>
+      </mesh> */}
 
-      {/* <ambientLight intensity={3} color={"#fffff"} /> */}
+      {/* <ambientLight intensity={3} color={"#fffff"} />
 
-      {/* <primitive object={model.scene} position={[0, -1, -4]} /> */}
+      <primitive object={model.scene} position={[0, -1, -4]} />
 
-      <Environment files={"./envMap.hdr"} />
+      <Environment files={"./envMap.hdr"} /> */}
+
+      <Instances>
+        <boxGeometry />
+        <meshMatcapMaterial matcap={matcap} />
+        {Array.from({length: 200}).map((_,i)=>{
+        return <Instance
+          key={i}
+          position={[
+            Math.random() * 50 - 25, 
+            Math.random() * 10 - 5, 
+            Math.random() * 50 - 25
+          ]}
+          scale={Math.random() * 0.5}
+          />
+        })}
+      </Instances>
     </>
   );
 };
